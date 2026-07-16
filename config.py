0195401e444f5598454f36531a64941231fdc930
teasmvd-arch <teasmvd@gmail.com>
2026-07-16 13:35:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY", "").strip()
 OPENSUBTITLES_API_KEY = os.getenv("OPENSUBTITLES_API_KEY", "").strip()
-
+SUBDL_API_KEY = os.getenv("SUBDL_API_KEY")
 
 def check_config():
     missing = []
