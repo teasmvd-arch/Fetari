4e488907dc28a239e5648a8f220221df17e00a05
teasmvd-arch <teasmvd@gmail.com>
2026-07-17 16:10:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -295,50 +295,48 @@ async def button_callback(
 
 
         return
-        # -------- FAVORITE --------
+        
+# -------- FAVORITE --------
 
-    if query.data.startswith("fav_"):
+if query.data.startswith("fav_"):
 
-        index = int(
-            query.data.replace(
-                "fav_",
-                ""
-            )
+    print("FAVORITE CLICKED:", query.data)
+
+    index = int(
+        query.data.replace(
+            "fav_",
+            ""
+        )
+    )
+
+    user_id = update.effective_user.id
+
+    movie = USER_TITLES[user_id][index]
+
+    saved = add_favorite(
+        user_id,
+        {
+            "id": movie["id"],
+            "title": movie.get("title") or movie.get("name"),
+            "poster": movie.get("poster"),
+            "media_type": movie.get("media_type"),
+        }
+    )
+
+
+    if saved:
+        await query.answer(
+            "❤️ Added to favorites!",
+            show_alert=True
         )
 
-        user_id = update.effective_user.id
-
-        movie = USER_TITLES[user_id][index]
-
-
-        saved = add_favorite(
-            user_id,
-            {
-                "id": movie["id"],
-                "title": (
-                    movie.get("title")
-                    or movie.get("name")
-                ),
-                "poster": movie.get("poster"),
-                "media_type": movie.get("media_type"),
-            }
+    else:
+        await query.answer(
+            "Already saved!",
+            show_alert=True
         )
 
-
-        if saved:
-
-            await query.answer(
-                "❤️ Added to favorites!"
-            )
-
-        else:
-
-            await query.answer(
-                "Already saved!"
-            )
-
-
-        return
+    return
 
 
 
